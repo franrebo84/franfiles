f6f26b1fe1ea55a8b34cd2f73f627a0a83e1706c
Francisco Rebolledo <FranciscoRebolledo@goodhabitz.com>
2022-08-29 15:06:57 +0200

    fw file edited

--- firewall.tf
+++ firewall.tf
@@ -1,31 +1,3 @@
-#resource "google_compute_firewall" "allow-ingress-traffic-from-vpn" {
-#  name        = "allow-ingress-traffic-to-vpn"
-#  network     = google_compute_network.hub.name
-#  project     = data.google_project.hub.project_id
-#
-#  allow {
-#    protocol  = "tcp"
-#  }
-#
-#  source_ranges = [var.on_premise_network_ip_range]
-#  priority    = 1000
-#  direction   = "INGRESS"
-#}
-
-# resource "google_compute_firewall" "allow-egress-traffic-to-vpn" {
-  # name        = "allow-egress-traffic-to-vpn"
-  # network     = google_compute_network.hub.name
-  # project     = data.google_project.hub.project_id
-# 
-  # allow {
-    # protocol  = "tcp"
-  # }
-# 
-  # destination_ranges = [var.on_premise_network_ip_range]
-  # priority    = 1000
-  # direction   = "EGRESS"
-# }
-
 resource "google_compute_firewall" "deny-ingress-traffic-from-internet" {
   name    = "deny-all-ingress-traffic"
   network = google_compute_network.hub.name
